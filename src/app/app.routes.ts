2369b0ffe0b34a890ff6e06d2af264e684ffc2a4
fabisrmz <fabioligua0208@gmail.com>
2026-05-27 09:51:16 -0600

    fix: se agrego todo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomePage } from './pages/home';
 
 
+
 export const routes: Routes = [
-    { path: '', component: HomePage }
+    { path: '', component: HomePage },
+
 ];
