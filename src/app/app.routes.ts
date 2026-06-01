e63cec4ef829726b4f7be534eed9ff0e13976310
Brawnie <braulioyaelvargas@gamil.com>
2026-06-01 08:50:42 -0600

    modificacion de home para ver la tabla productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home';
-
-
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
-    { path: '', component: HomePage },
-
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'productos',
+    component: Products
+  }
 ];
